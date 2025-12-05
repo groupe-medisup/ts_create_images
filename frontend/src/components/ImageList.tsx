659e5291299e5c7373cce788448b00a79c79218e
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2025-12-05 17:49:29 +0100

    style: increase image size

--- frontend/src/components/ImageList.tsx
+++ frontend/src/components/ImageList.tsx
@@ -13,7 +13,7 @@ function ImageSubList({ images, deleteImage }: ImageListProps) {
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+        gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))",
         gap: "20px",
         marginTop: "20px",
       }}
